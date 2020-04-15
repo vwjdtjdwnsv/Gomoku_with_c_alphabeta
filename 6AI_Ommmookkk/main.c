197ac14add745d77a9e6df439f2cb134b4bb2562
kamellot <vwjdtjdwnsv@gmail.com>
2020-04-15 21:08:57 +0900

    Initial Commit

---
+++ 6AI_Ommmookkk/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  6AI_Ommmookkk
+//
+//  Created by 정성준 on 2020/04/15.
+//  Copyright © 2020 정성준. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
